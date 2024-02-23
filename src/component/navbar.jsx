48a737a83e047c3e6923ac2230120cc33370c220
Joezy12 <oyebodejoseph74@gmail.com>
2024-02-22 17:08:34 -0800

    latest commit

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import redbull from '../../public/redbull.png'
 
 function Navbar() {
 
+    const [openNav, setOpenNav] = useState(false);
+
+    const deco = {
+        transform: openNav ? `translate(0px, 0px)`: `translate(-400px, 0px)`,
+    }
+
+    function open() {
+        setOpenNav((prev)=> {
+            return !prev;
+        })
+    }
+
     return (
-        <section>
-            <div className="navbar">
-                <div className='logo'><img src={redbull} alt="" /><h2>BigBankFX</h2></div>
+        <section className='total-navbar'>
+            <div className="navbar navbar1">
+                <div className='logo logo1'><img src={redbull} alt="" /><h2>BigBankFX</h2></div>
                 <div className="nav-links">
                     <ul>
                         <li>About US</li>
@@ -17,12 +30,28 @@ function Navbar() {
                 </div>
                 <div className="sign-btns">
                     <button className="login-btn">Login</button>
-                    <button>Sign Up</button>
+                    <button className="sign-btn">Sign Up</button>
+                </div>
+            </div>
+            <div className="navbar navbar2" style={deco}>
+                <div className='logo logo1'><img src={redbull} alt="" /><h2>BigBankFX</h2></div>
+                <div className="nav-links">
+                    <ul>
+                        <li>About US</li>
+                        <li>Our Service</li>
+                        <li>Legal Information</li>
+                        <li>Help Center</li>
+                        <li>Terms of Use</li>
+                    </ul>
+                </div>
+                <div className="sign-btns">
+                    <button className="login-btn">Login</button>
+                    <button className="sign-btn">Sign Up</button>
                 </div>
             </div>
             <div className='phone-nav'>
-            <div className='logo'><img src={redbull} alt="" /><h2>BigBankFX</h2></div>
-            <div><i className='bi-list'></i></div>
+                <div className='logo'><img src={redbull} alt="" /><h2>BigBankFX</h2></div>
+                <div onClick={open}>{openNav ? <i className='bi-x'></i>: <i className='bi-list'></i>}</div>
             </div>
         </section>
     )
